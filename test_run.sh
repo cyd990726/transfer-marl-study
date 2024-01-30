97ff37894e017cbb3d21b764c0688d0b3ca4aec8
cyd小白 <73676948+cyd990726@users.noreply.github.com>
2024-01-30 16:49:12 +0800

    Add files via upload

---
+++ test_run.sh
@@ -0,0 +1,4 @@
+export CUDA_VISIBLE_DEVICES=1
+python src/main.py --config=xtrans_train_beta --task-config=marine_battle --remark=bm
+# python src/main.py --config=xtrans_train_beta --task-config=stalkers_and_zealots --remark=bm
+# python src/main.py --config=xtrans_train_mpe_beta --task-config=grid_mpe --remark=bm
